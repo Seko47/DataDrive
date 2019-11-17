e8aebfee1b217450ee0d5b38ca370875ea4dcaac
Rafał Sekular <rafalsekular96@gmail.com>
2019-11-17 23:36:25 +0100

    fix

--- DataDrive/ClientApp/src/app/app.component.ts
+++ DataDrive/ClientApp/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { PwaService } from './services/pwa.service';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    providers: [PwaService]
 })
 export class AppComponent {
-    title = 'app';
+    title = 'DataDrive';
 
-    constructor(private translate: TranslateService) {
+    constructor(private translate: TranslateService, public pwaService: PwaService) {
         let language = 'en';
         if (localStorage.getItem("application_language")) {
             language = localStorage.getItem("application_language");
@@ -19,5 +21,9 @@ export class AppComponent {
     public useLanguage(language: string) {
         localStorage.setItem("application_language", language);
         this.translate.use(language);
+    }
+
+    public installPwa(): void {
+        this.pwaService.promptInstallEvent.prompt();
     }
 }
