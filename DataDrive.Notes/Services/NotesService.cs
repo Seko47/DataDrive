49ff7bb398170b38e7da543a810ecb3d884ff1b5
Rafał Sekular <rafalsekular96@gmail.com>
2019-11-27 10:06:26 +0100

    add class library for notes

---
+++ DataDrive.Notes/Services/NotesService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDrive.Notes.Services
+{
+    public class NotesService
+    {
+    }
+}
---
+++ DataDrive.Tests/DataDrive.Notes/Services/NotesServiceTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDrive.Tests.DataDrive.Notes.Services
+{
+    public class NotesServiceTest
+    {
+    }
+}
