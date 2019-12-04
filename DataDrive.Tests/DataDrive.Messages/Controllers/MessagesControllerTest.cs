5a6a73ab69f081e937c41d0bbff87163d1b76f80
Rafał Sekular <rafalsekular96@gmail.com>
2019-12-04 11:26:09 +0100

    add test classes for message

---
+++ DataDrive.Tests/DataDrive.Messages/Controllers/MessagesControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataDrive.Tests.DataDrive.Messages.Controllers
+{
+    public class MessagesControllerTest
+    {
+    }
+}
